1c47dbe3f41f06560398548835ca706ca7aa0ee1
Nicholas Cheng <74994367+Miniongolf@users.noreply.github.com>
2026-02-01 11:52:05 -0500

    implement line follower

--- include/line_following.hpp
+++ include/line_following.hpp
@@ -3,6 +3,14 @@
 
 #include "devices.hpp"
 
-void followLine(DDBot& drivetrain, ColorSensor& colorSensor, ColorName lineColor, int bias = 0);
+/**
+ * @brief Follow a line of a specified color using the color sensor
+ * @param drivetrain Reference to the DDBot drivetrain object
+ * @param colorSensor Reference to the ColorSensor object
+ * @param lineColor The color of the line to follow
+ * @param trackLeft If true, the robot will track the line on the left side of the robot
+ *                  Otherwise, it will track the line on the right side
+ */
+void followLine(DDBot& drivetrain, ColorSensor& colorSensor, ColorName lineColor, bool trackLeft = true);
 
 #endif // LINE_FOLLOWING_HPP
--- src/utils/line_following.cpp
+++ src/utils/line_following.cpp
@@ -1,5 +1,19 @@
 #include "devices.hpp"
 
+void followLine(DDBot& drivetrain, ColorSensor& colorSensor, ColorName lineColor, bool trackLeft) {
+  const int baseSpeed = 150;
+  const int correctionSpeed = 100;
 
+  ColorRGB colorReading = colorSensor.readNormalized();
+  ColorName detectedColor = colorSensor.classify(colorReading);
 
+  bool onLine = (detectedColor == lineColor);
 
+  if (trackLeft) {
+    (onLine) ? drivetrain.right(255) : drivetrain.left(255);
+  } else {
+    (onLine) ? drivetrain.left(255) : drivetrain.right(255);
+  }
+
+  delay(50);
+}
