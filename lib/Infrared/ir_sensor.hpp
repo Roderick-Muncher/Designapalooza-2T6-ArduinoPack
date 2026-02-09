659267593dfe5d443d05d013ff69a349d2c656ca
Nicholas Cheng <74994367+Miniongolf@users.noreply.github.com>
2026-02-09 14:44:01 -0500

    feat: ir sensor utils

---
+++ lib/Infrared/ir_sensor.hpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+class InfraredSensor {
+ public:
+  InfraredSensor(uint8_t pin) : _pin(pin) { pinMode(_pin, INPUT); }
+
+  bool detectsObject() { return digitalRead(_pin) == HIGH; }
+
+  bool onDarkLine() { return digitalRead(_pin) == LOW; }
+
+ protected:
+  uint8_t _pin;
+};
